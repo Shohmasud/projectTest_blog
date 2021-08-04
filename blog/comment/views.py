8bb341f937bff9d927516b22f667a884f10f5877
Shohmasud <name@mail>
2021-08-04 19:24:30 +0700

    Добавление класса PostListView вывода html файла и отпрвки данных в форму

--- blog/comment/views.py
+++ blog/comment/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+#/// Необходиммые модули для работы приложения из файла personalModul.py
 
-# Create your views here.
+#/// Rest Api методом GET
+from .personalModul import  *
+
+
+"""" Класс описывающий вывод html элементов
+    и отпраку данных в форму и её вывод """
+class PostListView(View):
+    # /// При вызове этого метода выводим необходимые данные
+    def get(self, request):
+        # Пишем обработчика ошибки , при удалении самой верней вершины дерево комментарий только его
+        # возникновении ошибки mptt модуля
+        try:
+            # /// Выполняет функцию первоначального входа на страничку приложения
+            first_visitSite = 'False'
+
+            #/// Экземпляры формы
+            form = PostForm()
+            form_child = PostFormChild()
+
+            #/// Получаем название нужного блога которое нужно вывести ,если установлен True
+            blog_names = blog_name()
+            blog_texts = blog_text()
+
+            # ///Модель комментов которое передаётся в mptt тег, для вывода дерево
+            comments = Comments.objects.all()
+            return render(request, 'post_list.html', {'form': form, 'comments': comments,
+                                                      'form_child': form_child, 'i': first_visitSite,
+                                                      'blog_name': blog_name, 'blog_text': blog_text})
+        #// Иначе заново перезапускаемся
+        except:
+            delete = Comments.objects.all().delete()
+            return redirect('post-list')
+
+    # /// При вызове этого метода сохраняеем отправленые элементы в базу
+    def post(self, request):
+        form_child = PostFormChild()
+
+        #/// Получаем данные из формы
+        first_visitSite = 'True'
+        form = PostForm(request.POST)
+
+        #/// Если данные валидны то сохроняем полученные данные из формы в базу данных
+        if form.is_valid():
+            body = request.POST['body']
+            createChild = Comments.objects.create(body=body,parent=None).save()
+
+            # ///Модель комментов которое передаётся в mptt тег, для вывода дерево
+            comment = Comments.objects.all()
+            # /// Получаем название нужного блога которое нужно вывести ,если установлен True
+            blog_names = blog_name()
+            blog_texts = blog_text()
+            return render(request, 'post_list.html', {'form': form,'comments':comment,
+                                                      'form_child':form_child,'i': first_visitSite,
+                                                      'blog_name':blog_name, 'blog_text':blog_text})
+        pass
+

1f4cde6609e4fe3e17f034b98a2097866b6aa014
Shohmasud <name@mail>
2021-08-04 19:07:05 +0700

    Подключение ранее созданных функций в файле util.py

--- blog/comment/personalModul.py
+++ blog/comment/personalModul.py
@@ -2,6 +2,7 @@ from django.views.generic.edit import UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import View
+from .forms import PostForm,PostFormChild
 from .models import Comments,Blog
 from rest_framework import status
 from psycopg2 import IntegrityError
@@ -9,3 +10,4 @@ from  rest_framework.views import APIView
 from django.shortcuts import render, redirect
 from  rest_framework.response import Response
 from django.http import HttpResponse, HttpResponseRedirect
+from .utils import blog_name,blog_text

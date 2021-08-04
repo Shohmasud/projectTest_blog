77e7be9a526b75282093bf24d2b4d89458c7a435
Shohmasud <name@mail>
2021-08-04 19:10:03 +0700

    Создание классов модели для их отображения в базе данных

--- blog/comment/admin.py
+++ blog/comment/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Comments,Blog
 # Register your models here.
+class Admin_Blog(admin.ModelAdmin):
+    list_display = ('id', 'nameBlog','textBlog','showBlog')
+    search_fields = ['id','nameBlog','textBlog','showBlog ']
+admin.site.register(Blog,Admin_Blog)
+
+class Admin_Comment(admin.ModelAdmin):
+    list_display = ('id', 'body','created_on','parent',)
+    search_fields = ['id','body','created_on']
+admin.site.register(Comments, Admin_Comment)
